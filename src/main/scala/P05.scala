6b08b7cb0185f1dcef84505ae75e2b156fbe10a8
Remy D'Agostino <remy.dagostino@soundcloud.com>
2016-09-10 13:31:54 +0200

    Fix potential performance whoopsie with #reverse

--- src/main/scala/P05.scala
+++ src/main/scala/P05.scala
@@ -2,5 +2,5 @@ package com.remydagostino.s99
 
 object P05 {
   def reverse[A](list: List[A]): List[A] =
-    list.foldRight(List.empty[A]) { (x, acc) => acc :+ x }
+    list.foldLeft(List.empty[A]) { (acc, x) => x :: acc }
 }
